b2e5959342577ccace0605b183e2cc1d35ddcd8b
Brandon Martel <brandonmartel@gmail.com>
2020-08-24 19:48:37 -0500

    add cssnano

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,10 @@
-module.exports = {
-  plugins: [require('tailwindcss'), require('autoprefixer')],
-};
+const tailwind = require('tailwindcss');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+
+const plugins =
+  process.env.NODE_ENV === 'production'
+    ? [tailwind, autoprefixer, cssnano]
+    : [tailwind];
+
+module.exports = { plugins };
