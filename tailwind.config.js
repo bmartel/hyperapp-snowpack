7d6097f866477ea53bc51c48fa47411adb0e849f
Brandon Martel <brandonmartel@gmail.com>
2020-08-24 19:37:52 -0500

    adding tailwind + postcss

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  future: {
+    removeDeprecatedGapUtilities: true,
+  },
+  purge: ['./public/**/*.html', './src/**/*.js', './src/**/*.jsx'],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+};
