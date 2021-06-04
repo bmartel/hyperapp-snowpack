405d4a4e0754017a7aabda8730663a276d250c2f
Brandon Martel <brandonmartel@gmail.com>
2021-06-03 21:47:43 -0500

    updating the snowpack config

--- snowpack.config.mjs
+++ snowpack.config.mjs
@@ -2,14 +2,8 @@ import config from '@martel/hyperapp-scripts-snowpack';
 
 export default {
   ...config,
-  optimize: {
-    entrypoints: 'auto',
-    bundle: true,
-    treeshake: true,
-    minify: true,
-    target: 'es2018',
-  },
   plugins: [
+    ...config.plugins,
     [
       '@snowpack/plugin-build-script',
       { cmd: 'postcss', input: ['.css'], output: ['.css'] },
