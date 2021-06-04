0c807e7e72864ba6a973c66415e09dcd72cda969
Brandon Martel <brandonmartel@gmail.com>
2021-06-03 20:20:58 -0500

    updating the configs and dependencies to latest

--- src/App.ts
+++ src/App.ts
@@ -1,18 +1,18 @@
-import { text } from 'hyperapp';
-import { a, p, header, img, div, code } from '@hyperapp/html';
+import { h, text } from 'hyperapp';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return div({ class: 'App' }, [
-    header({ class: 'App-header' }, [
-      img({ src: logo, class: 'App-logo', alt: 'logo' }),
-      p({}, [
+  return h('div', { class: 'App' }, [
+    h('header', { class: 'App-header' }, [
+      h('img', { src: logo, class: 'App-logo', alt: 'logo' }),
+      h('p', {}, [
         text('Edit '),
-        code({}, text('src/App.js')),
+        h('code', {}, text('src/App.js')),
         text(' and save to reload'),
       ]),
-      a(
+      h(
+        'a',
         {
           href: 'https://hyperapp.dev',
           class: 'App-link',
